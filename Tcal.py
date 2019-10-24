23e528af4b8416ec335edefc8c1311340aedce21
Rudra shah <51542683+scienceLabwork@users.noreply.github.com>
2019-10-24 20:52:35 +0530

    Update Tcal.py

--- Tcal.py
+++ Tcal.py
@@ -1,10 +1,12 @@
-numberOf = input('TABLE OF WHICH NUMBER? ')
-
-if numberOf.isdigit()==True:
-    for x in range(0,11):
-        y = (int(numberOf) * x)
-        print(numberOf,'x',x,'=',y)
-else:
-    print('Please input a Number')
+x = 'Y'
+while x=='Y':
+    numberOf = input('TABLE OF WHICH NUMBER? ')
+    if numberOf.isdigit()==True:
+        for x in range(0,11):
+            y = (int(numberOf) * x)
+            print(numberOf,'x',x,'=',y)
+    else:
+        print('Please input a Number')
+    x = input('Want to get table of another number(Y/n) ')
     
 print('Tables calculator by Rudra shah')
