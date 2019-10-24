47f7841e1e8dde6754a0f3ff80e571497b5d4216
Rudra shah <51542683+scienceLabwork@users.noreply.github.com>
2019-10-24 20:14:32 +0530

    Add files via upload

---
+++ table's calculator.py
@@ -0,0 +1,8 @@
+numberOf = input('TABLE OF WHICH NUMBER? ')
+
+if numberOf.isdigit()==True:
+    for x in range(0,11):
+        y = (int(numberOf) * x)
+        print(numberOf,'x',x,'=',y)
+else:
+    print('Please input a Number')
